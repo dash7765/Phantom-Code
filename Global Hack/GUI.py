3f050eb60ab49e0143dee536834c549203d0177a
David Snider <dsnider.techie@gmail.com>
2016-10-22 17:02:53 -0500

    yes this is good right now
    adding more

---
+++ Global Hack/GUI.py
@@ -0,0 +1,16 @@
+#from Hackathon import gender_chart
+import tkinter
+
+def break_fix_gender():
+    '''fixes the problem with importing'''
+    import Hackathon
+    Hackathon.gender_chart()
+        
+top = tkinter.Tk()
+top.width = 600
+top.height = 600
+top.grid()
+tkinter.Button(top, bg='orange',text='Gender Chart',
+               command=break_fix_gender).grid(row=0,column=0)
+
+top.mainloop()
