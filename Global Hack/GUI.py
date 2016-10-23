c8d48edbf02459c8c9720ff9051d9c5227cc6ec3
David Snider <dsnider.techie@gmail.com>
2016-10-23 01:12:54 -0500

    GUI

--- Global Hack/GUI.py
+++ Global Hack/GUI.py
@@ -3,16 +3,52 @@
 
 import tkinter
 
-def break_fix_gender():
+def button_gender():
     '''fixes the problem with importing'''
     import Hackathon
     Hackathon.gender_chart()
-        
+
+def button_veteran_status():
+    '''fixes the problem with importing'''
+    import Hackathon
+    Hackathon.veteran_status_chart()
+
+def button_race():
+    '''fixes the problem with importing'''
+    import Hackathon
+    Hackathon.race_chart()
+
+def button_employment():
+    '''fixes the problem with importing'''
+    import Hackathon
+    Hackathon.employment_chart()
+    
+def button_prior_residence():
+    '''fixes the problem with importing'''
+    import Hackathon
+    Hackathon.prior_residence_chart()
+    
+def button_domestic_abuse():
+    '''fixes the problem with importing'''
+    import Hackathon
+    Hackathon.domestic_abuse_chart()
+    
+    
 top = tkinter.Tk()
 top.width = 600
 top.height = 600
 top.grid()
 tkinter.Button(top, bg='orange',text='Gender Chart',
-               command=break_fix_gender).grid(row=0,column=0)
+               command=button_gender).grid()
+tkinter.Button(top, bg='orange',text='Veteran Status Chart',
+               command=button_veteran_status).grid()
+tkinter.Button(top, bg='orange',text='Race Chart',
+               command=button_race).grid()
+tkinter.Button(top, bg='orange',text='Employment Chart',
+               command=button_employment).grid()
+tkinter.Button(top, bg='orange',text='Prior Residence Chart',
+               command=button_prior_residence).grid()
+tkinter.Button(top, bg='orange',text='Domestic Abuse Chart',
+               command=button_domestic_abuse).grid()
 
 top.mainloop()
